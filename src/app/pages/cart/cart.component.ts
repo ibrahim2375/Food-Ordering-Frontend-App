8bdebae036119e6a66d67e5957cfd9a2826bdb4d
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-04-30 02:31:14 +0300

    up

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+//icons
+import {faTrash } from '@fortawesome/free-solid-svg-icons';
+import { CartService } from 'src/app/services/cart.service';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from 'src/app/shared/models/CartItem';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+ //icons
+faTrash = faTrash;
+cart!:Cart;
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartService.getCartObservable().subscribe(cart => {
+      this.cart = cart;
+    })
   }
-
+  removeFromCart(cartItem:CartItem){
+  this.cartService.removeFromCart(cartItem.food.id);
+  }
+  changeItemQuantity(cartItem:CartItem,quantity:string){
+    const quantityNumber = parseInt(quantity);
+    this.cartService.changeItemQuantity(cartItem.food.id, quantityNumber);
+  }
 }
--- src/app/pages/food/food.component.ts
+++ src/app/pages/food/food.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from 'src/app/services/food.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Food } from 'src/app/shared/models/Food';
 //icons
 import { faHeart,faClock } from '@fortawesome/free-solid-svg-icons';
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
@@ -12,7 +13,7 @@ import { faHeart,faClock } from '@fortawesome/free-solid-svg-icons';
 export class FoodComponent implements OnInit {
   faHeart = faHeart
   food!:Food;
-  constructor(private activateRoute: ActivatedRoute,private foodService:FoodService) { }
+  constructor(private activateRoute: ActivatedRoute,private foodService:FoodService,private router:Router,private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activateRoute.params.subscribe((param) => {
@@ -20,6 +21,11 @@ export class FoodComponent implements OnInit {
         this.food = this.foodService.getFoodById(param['id']);
       }
     })
+  }
+  // add to cart
+  addToCart(){
+  this.cartService.addToCart(this.food);
+  this.router.navigateByUrl('/cart');
   }
 
 }
