73411815601f0bbb8eefd4bc5f60dc4b5673df0e
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-04-29 23:38:19 +0300

    handel navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { FoodComponent } from './pages/food/food.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'search/:query',component:HomeComponent},
   {path:'tag/:tag',component:HomeComponent},
   {path:'food/:id',component:FoodComponent},
-  {path:'cart',component:FoodComponent},
+  {path:'cart',component:CartComponent},
   { path: '**', component: ErrorComponent }, //should be always last route path
 ];
 
