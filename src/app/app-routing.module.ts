f98a16dc7a44d481c5c967935125d7ae4585793d
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-04-29 23:18:12 +0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { FoodComponent } from './pages/food/food.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'search/:query',component:HomeComponent},
+  {path:'tag/:tag',component:HomeComponent},
+  {path:'food/:id',component:FoodComponent},
+  { path: '**', component: ErrorComponent }, //should be always last route path
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './components/search/search.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { FoodComponent } from './pages/food/food.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    ErrorComponent,
+    TagsComponent,
+    FoodComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RatingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
