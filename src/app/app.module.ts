13f070c874177761995925cc5c2449248e94068b
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-04-30 13:37:46 +0300

    httpclient mudel added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +35,9 @@ import { FoodsComponent } from './pages/foods/foods.component';
     AppRoutingModule,
     FontAwesomeModule,
     RatingModule,
-    FormsModule 
+    HttpClientModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
