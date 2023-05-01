5d4c0216154303a39a61244448d89ff55cb32b6d
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-05-01 20:37:47 +0300

    uo

--- src/app/shared/api/api.ts
+++ src/app/shared/api/api.ts
@@ -3,7 +3,7 @@ import { environment } from "src/environments/environment";
 const API = environment.api;
 
 export const FOODS_API = `${API}/api/foods`;
-export const FOODS_BY_ID = `${API}/api/food/`;
+export const FOODS_BY_ID = `${API}/api/foods/`;
 export const FOODS_BY_SEARCH = `${API}/api/foods/search/`;
 export const FOOD_TAGS = `${API}/api/foods/tags`;
 export const FOODS_BY_TAG = `${API}/api/foods/tag/`;
