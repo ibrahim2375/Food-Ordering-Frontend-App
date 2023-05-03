b7d573dffcd430258d217cc2473c91d632100c23
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-05-03 20:37:19 +0300

    trending section design

--- src/app/components/trending-item/trending-item.component.ts
+++ src/app/components/trending-item/trending-item.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-trending-item',
   templateUrl: './trending-item.component.html',
   styleUrls: ['./trending-item.component.css']
 })
 export class TrendingItemComponent implements OnInit {
-
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/trending-section/trending-section.component.ts
+++ src/app/components/trending-section/trending-section.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+//global variables form scroll by mouse
 
 @Component({
   selector: 'app-trending-section',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trending-section.component.css']
 })
 export class TrendingSectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  scrollLeft = 0;
+  startX=0;
+  isDown = false;
+  @ViewChild('items',{static:true}) elementRef!:ElementRef;
+  constructor() { 
+  
   }
 
+  ngOnInit(): void {
+    this.ScrollByMouse();
+  }
+
+ScrollByMouse = () => {
+    this.elementRef.nativeElement.addEventListener('mousedown', (e:any) => {
+        this.isDown = true;
+        //get start point in page
+        this.startX = e.pageX - this.elementRef.nativeElement.offsetLeft;
+        this.scrollLeft = this.elementRef.nativeElement.scrollLeft;
+    })
+    this.elementRef.nativeElement.addEventListener("mouseleave", () => {
+        this.isDown = false;
+    });
+    this.elementRef.nativeElement.addEventListener("mouseup", () => {
+        this.isDown = false;
+    });
+    this.elementRef.nativeElement.addEventListener("mousemove", (e:any) => {
+        //check if its down
+        if (!this.isDown) return;
+        // e.preventDefault();
+        if (this.isDown) {
+            //calculate cursor position
+            let currentX = e.pageX - this.elementRef.nativeElement.offsetLeft;
+            //minse from the start point
+            let moved = currentX - this.startX;
+            //assign moved value to scroll left
+            this.elementRef.nativeElement.scrollLeft = this.scrollLeft - moved;
+        }
+    });
+}
 }
