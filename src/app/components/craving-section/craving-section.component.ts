ec5d2a2556c49c5ad79f6fb08a5d920d3d79f8cf
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-05-04 17:40:50 +0300

    up

---
+++ src/app/components/craving-section/craving-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-craving-section',
+  templateUrl: './craving-section.component.html',
+  styleUrls: ['./craving-section.component.css']
+})
+export class CravingSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
